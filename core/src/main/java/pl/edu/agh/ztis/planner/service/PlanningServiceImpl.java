4093adb45a49cf9d5ee3517c8162737803d43093
Pharisaeus <spodgorski@gmail.com>
2013-05-23 23:25:52 +0200

    Added all measures to default webservice call

--- core/src/main/java/pl/edu/agh/ztis/planner/service/PlanningServiceImpl.java
+++ core/src/main/java/pl/edu/agh/ztis/planner/service/PlanningServiceImpl.java
@@ -1,16 +1,21 @@
 package pl.edu.agh.ztis.planner.service;
 
+import javax.jws.WebParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import pl.edu.agh.ztis.planner.executors.AsynchronousExecutor;
 import pl.edu.agh.ztis.planner.executors.task.PlanningJob;
 import pl.edu.agh.ztis.planner.mappers.ModelMapper;
+import pl.edu.agh.ztis.planner.measures.AggregateMeasure;
 import pl.edu.agh.ztis.planner.measures.ExecutionTimeMeasure;
+import pl.edu.agh.ztis.planner.measures.Measure;
+import pl.edu.agh.ztis.planner.measures.MemoryMeasure;
+import pl.edu.agh.ztis.planner.measures.PathLengthMeasure;
 import pl.edu.agh.ztis.planner.ws.GraphPlanningPortType;
 import pl.edu.agh.ztis.planner.ws.PlanningTask;
 import pl.edu.agh.ztis.planner.ws.PlanningTaskResponse;
-
-import javax.jws.WebParam;
 
 @Component
 public class PlanningServiceImpl implements GraphPlanningPortType {
@@ -26,8 +31,17 @@ public class PlanningServiceImpl implements GraphPlanningPortType {
             @WebParam(partName = "parameters", name = "planning-task", targetNamespace = "http://agh.edu.pl/ztis/planning") PlanningTask parameters) {
         PlanningJob planningJob = mapper.mapPlanningTask(parameters);
 
-        String status = executor.execute(planningJob, new ExecutionTimeMeasure());
+        Measure measure = getAllMeasures();
+        String status = executor.execute(planningJob, measure);
 
         return new PlanningTaskResponse().withStatus(status);
+    }
+
+    private Measure getAllMeasures() {
+        AggregateMeasure measure = new AggregateMeasure();
+        measure.addMeasure(new ExecutionTimeMeasure());
+        measure.addMeasure(new MemoryMeasure());
+        measure.addMeasure(new PathLengthMeasure());
+        return measure;
     }
 }
