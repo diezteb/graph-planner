65de6510bf8a4103e33f431140be02f9adb119f1
Pharisaeus <spodgorski@gmail.com>
2013-06-11 17:48:12 +0200

    changed view template to FTL

--- client/src/main/java/pl/edu/agh/ztis/client/controllers/MainController.java
+++ client/src/main/java/pl/edu/agh/ztis/client/controllers/MainController.java
@@ -2,10 +2,10 @@ package pl.edu.agh.ztis.client.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 import pl.edu.agh.ztis.client.GraphCreator;
 import pl.edu.agh.ztis.client.ServiceInvoker;
 import pl.edu.agh.ztis.planner.ws.PlanningTaskResponse;
@@ -22,7 +22,7 @@ public class MainController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ModelAndView printWelcome() {
-        ModelAndView mav = new ModelAndView("hello.jsp");
+        ModelAndView mav = new ModelAndView("hello");
         PlanningTaskResponse response = serviceInvoker.invoke(graphCreator.createGraph());
         mav.addObject("message", response.getStatus());
         return mav;
